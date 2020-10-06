cf63ed8c13a2d92aa6d1f696c050f628d588bdff
Laminne Tatsuto Yamamoto <laminne33569@approvers.dev>
2020-10-06 23:10:20 +0900

    Update main.rb

--- main.rb
+++ main.rb
@@ -3,6 +3,8 @@ require 'dotenv'
 
 Dotenv.load
 
+info_channel = ENV['CHANNEL_ID']
+
 bot = Discordrb::Commands::CommandBot.new(
     token: ENV['TOKEN'],
     client_id: ENV['CLIENT_ID'],
@@ -68,4 +70,16 @@ bot.command :about do |event|
     end
   end
 
+bot.voice_state_update do |event|
+    user = event.user.name
+
+    if event.channel == nil then
+        channel_name = event.old_channel.name
+        bot.send_message(info_channel, "```#{user} が #{channel_name}を出ました```")
+    else
+        channel_name = event.channel.name
+
+        bot.send_message(info_channel, " ```#{user} が #{channel_name}に入りました```")
+    end
+end
 bot.run
