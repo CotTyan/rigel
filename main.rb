b0f657a017c4d32cb12864f4800c5ca5f17b55f5
Laminne Tatsuto Yamamoto <laminne33569@approvers.dev>
2020-10-06 23:02:08 +0900

    Revert "Update main.rb"
    This reverts commit c5707fc7070cbbc6d0e68fa6e7a1bf875acb13d5.

--- main.rb
+++ main.rb
@@ -3,8 +3,6 @@ require 'dotenv'
 
 Dotenv.load
 
-info_channel = "667386724229447706"
-
 bot = Discordrb::Commands::CommandBot.new(
     token: ENV['TOKEN'],
     client_id: ENV['CLIENT_ID'],
@@ -70,16 +68,4 @@ bot.command :about do |event|
     end
   end
 
-bot.voice_state_update do |event|
-    user = event.user.name
-
-    if event.channel == nil then
-        channel_name = event.old_channel.name
-        bot.send_message(info_channel, "```#{user} が #{channel_name}を出ました```")
-    else
-        channel_name = event.channel.name
-
-        bot.send_message(info_channel, " ```#{user} が #{channel_name}に入りました```")
-    end
-end
 bot.run
