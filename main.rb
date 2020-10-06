0f8b9c3dc59813db09a6530acf9ca045fbf2eec9
Laminne Tatsuto Yamamoto <laminne33569@approvers.dev>
2020-10-06 21:30:01 +0900

    コマンドの追加

--- main.rb
+++ main.rb
@@ -14,4 +14,58 @@ bot.command :hello do | message |
 end
 
 
+bot.command :about do |event|
+    event.send_embed do |embed|
+      embed.title = "About Rigel"
+      embed.url = "https://github.com/laminne/rigel"
+      embed.colour = 0x00ccff
+      embed.description = "**CotTyanをRuby(Discordrb)で書き直すプロジェクト**"
+      embed.footer = Discordrb::Webhooks::EmbedFooter.new(
+        text: "このボットのソースコードはMITライセンスの元、オープンソースで公開されています",
+      )
+      embed.author = Discordrb::Webhooks::EmbedAuthor.new(
+        name: 'Laminne',
+        url: 'https://github.com/laminne',
+        icon_url: 'https://github.com/laminne.png'
+      )
+    end
+  end
+
+  bot.command :LICENSE do |event|
+    event.send_embed do |embed|
+      embed.title = "LICENSE"
+      embed.url = "https://github.com/laminne/rigel"
+      embed.colour = 0x00ccff
+      embed.description = "```MIT License
+
+      Copyright (c) 2020 Tatsuto Laminne Yamamoto
+      
+      Permission is hereby granted, free of charge, to any person obtaining a copy
+      of this software and associated documentation files (the \"Software\"), to deal
+      in the Software without restriction, including without limitation the rights
+      to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+      copies of the Software, and to permit persons to whom the Software is
+      furnished to do so, subject to the following conditions:
+      
+      The above copyright notice and this permission notice shall be included in all
+      copies or substantial portions of the Software.
+      
+      THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+      IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+      FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+      AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+      LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+      OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+      SOFTWARE.```"
+      embed.footer = Discordrb::Webhooks::EmbedFooter.new(
+        text: "詳しくはプロジェクトページをご覧ください。",
+      )
+      embed.author = Discordrb::Webhooks::EmbedAuthor.new(
+        name: 'Laminne',
+        url: 'https://github.com/laminne',
+        icon_url: 'https://github.com/laminne.png'
+      )
+    end
+  end
+
 bot.run
